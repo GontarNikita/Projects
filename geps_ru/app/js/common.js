7f3d151883febcf67a7825296cc8ee997546241c
gem install bundler <stativka291@gmail.com>
2017-08-04 11:34:24 +0300

    Project geps.ru

---
+++ geps_ru/app/js/common.js
@@ -0,0 +1,49 @@
+$(function() {
+
+	//SVG Fallback
+	if(!Modernizr.svg) {
+		$("img[src*='svg']").attr("src", function() {
+			return $(this).attr("src").replace(".svg", ".png");
+		});
+	};
+
+	//E-mail Ajax Send
+	//Documentation & Example: https://github.com/agragregra/uniMail
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+	//Chrome Smooth Scroll
+	try {
+		$.browserSelector();
+		if($("html").hasClass("chrome")) {
+			$.smoothScroll();
+		}
+	} catch(err) {
+
+	};
+
+	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+
+});
+
+$(".characteristic_text a").on("click", function(e){
+	$(".characteristic_text a").removeClass("active_price");
+	if($(this).hasClass("active_price")){
+		$(this).removeClass("active_price");
+	}else{
+		$(this).addClass("active_price");
+	}
+})
